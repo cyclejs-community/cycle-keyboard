64134c60f0d42555f1008698dfde51c3e55d43a0
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-07-05 20:26:12 +0530

    Fixed export

---
+++ bundle.js
@@ -0,0 +1,1 @@
+console.error("Error: ENOENT: no such file or directory, open 'c:\\Users\\Sudarsan Balaji\\Source\\Repos\\cycle-keyboard\\cycle-keyboard\\.babelrc' while parsing file: c:\\Users\\Sudarsan Balaji\\Source\\Repos\\cycle-keyboard\\cycle-keyboard\\index.js");
--- index.js
+++ index.js
@@ -14,3 +14,5 @@ function makeKeyboardDriver() {
   };
   return keyboardDriver;
 }
+
+module.exports = { makeKeyboardDriver };
