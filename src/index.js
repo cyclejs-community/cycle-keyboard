ba3385c38321f5eb3fdb6da61c373479f5d2e978
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-07-05 20:45:04 +0530

    Fixed bug

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ function makeKeyboardDriver() {
   const keyDown$ = xs.create();
   const keyUp$ = xs.create();
   const keyPress$ = xs.create();
-  document.addEventListener('keydown', event => event$.shamefullySendNext(event));
-  document.addEventListener('keyup', event => event$.shamefullySendNext(event));
-  document.addEventListener('keypress', event => event$.shamefullySendNext(event));
+  document.addEventListener('keydown', event => keyDown$.shamefullySendNext(event));
+  document.addEventListener('keyup', event => keyUp$.shamefullySendNext(event));
+  document.addEventListener('keypress', event => keyPress$.shamefullySendNext(event));
   const keyboardDriver = {
     down$: keyDown$,
     up$: keyUp$,
