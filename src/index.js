0c518a33f7e7b2facfe00782299728548d842e9e
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-07-08 20:08:52 +0530

    Created classes and stuff

--- src/index.js
+++ src/index.js
@@ -4,114 +4,106 @@ import { getDisplayKey, getDisplayChar } from './utils';
 
 function makeKeyboardDriver() {
 
-  const KeyboardEventProducer = function (type, mapper) {
-    return {
-      start: function (listener) {
-        this.stream = listener;
-        document.addEventListener(type, this.handler);
-      },
-      stop: function () {
-        document.removeEventListener(type, this.handler);
-        this.stream = null;
-      },
-      stream: null,
-      handler: function (event) {
-        if (this.stream)
-          this.stream.next(mapper(event));
-      }
+  class KeyboardEventProducer {
+    constructor(type, mapper) {
+      
+      // hold onto this reference
+      const _this = this;
+
+      this.start = function (listener) {
+        _this.stream = listener;
+        document.addEventListener(type, _this.handler);
+      };
+      this.stop = function () {
+        document.removeEventListener(type, _this.handler);
+        _this.stream = null;
+      };
+      this.stream = null;
+      this.handler = event => _this.stream.next(mapper(event));
     }
   }
 
-  const StreamListener = function(producer) {
-    return {
-        next: ev => {
-          if(producer && producer.stream)
-            producter.stream.next(ev);
-        },
-        error: err => console.error(err),
-        complete: () => { }
-      }
+  class StreamListener {
+    constructor(producer) {
+      this.next = ev => producer.stream.next(ev),
+      this.error = err => console.error(err),
+      this.complete = () => { }
+    }
   }
 
-  const KeyboardStatusProducer = function (stream) {
-    const producer = {};
-    Object.assign(producer, {streamListener: StreamListener(producer)});
-    Object.assign(producer,
-    {
-      start: function (listener) {
-        this.stream = listener;
-        this.source$.addListener(this.streamListener);
-      },
-      stop: function () {
-        this.source$.removeListener(this.streamListener);
-      },
-      streamListener: StreamListener(this),
-      stream: null,
-      source$: stream
-    });
-    return producer;
+  class KeyboardStatusProducer {
+    constructor(stream) {
+      const _this = this;
+      this.stream = null;
+      this.source$ = stream;
+      this.streamListener = new StreamListener(this);
+      this.start = function (listener) {
+        _this.stream = listener;
+        _this.source$.addListener(_this.streamListener);
+      };
+      this.stop = () => _this.source$.removeListener(_this.streamListener);
+    }
   }
 
-  const keyDownEventProducer = KeyboardEventProducer('keydown',
+  const keyDownEventProducer = new KeyboardEventProducer('keydown',
     event => Object.assign(event, {
       displayKey: getDisplayKey(event)
     }));
 
-  const keyUpEventProducer = KeyboardEventProducer('keyup',
+  const keyUpEventProducer = new KeyboardEventProducer('keyup',
     event => Object.assign(event, {
       displayKey: getDisplayKey(event)
     }));
 
-  const keyPressEventProducer = KeyboardEventProducer('keypress',
+  const keyPressEventProducer = new KeyboardEventProducer('keypress',
     event => Object.assign(event, {
       displayKey: getDisplayKey(event),
       displayChar: getDisplayChar(event)
     }));
 
+  class KeyboardDriver {
+    constructor() {
+      const _this = this;
+      this.keyDown$ = xs.create(keyDownEventProducer);
+      this.keyUp$ = xs.create(keyUpEventProducer);
+      this.keyPress$ = xs.create(keyPressEventProducer);
+      const shiftProducer = new KeyboardStatusProducer(
+        xs.merge(
+          _this.keyDown$
+            .filter(e => e.displayKey == 'shift')
+            .map(e => true),
+          _this.keyUp$
+            .filter(e => e.displayKey == 'shift')
+            .map(e => false)
+        ).startWith(null));
+      this._capsLock = null;
+      const capsLockProducer = new KeyboardStatusProducer(
+        xs.merge(
+          _this.keyDown$
+            .filter(e => _this._capsLock != null && e.displayKey == 'caps lock')
+            .map(e => {
+              _this._capsLock = !_this._capsLock;
+              return capsLock;
+            }),
+          _this.keyPress$
+            .filter(e => {
+              var chr = getDisplayChar(e);
+              if (!chr || chr.toLowerCase() == chr.toUpperCase())
+                return false;
+              return true;
+            }).map(e => {
+              var chr = getDisplayChar(e);
+              _this.capsLock = (chr.toLowerCase() == chr && e.shiftKey) || (chr.toUpperCase() == chr && !e.shiftKey);
+              return _this._capsLock;
+            })
+        ).startWith(_this._capsLock));
+      this.shift$ = xs.create(shiftProducer);;
+      this.capsLock$ = xs.create(capsLockProducer);
+    }
+  }
+
   function keyboardDriver() {
-    const keyDown$ = xs.create(keyDownEventProducer);
-    const keyUp$ = xs.create(keyUpEventProducer);
-    const keyPress$ = xs.create(keyPressEventProducer);
-    const shiftProducer = KeyboardStatusProducer(
-      xs.merge(
-        keyDown$
-          .filter(e => e.displayKey == 'shift')
-          .map(e => true),
-        keyUp$
-          .filter(e => e.displayKey == 'shift')
-          .map(e => false)
-      ).startWith(null));
-    var capsLock = null;
-    const capsLockProducer = KeyboardStatusProducer(
-      xs.merge(
-        keyDown$
-          .filter(e => capsLock != null && e.displayKey == 'caps lock')
-          .map(e => {
-            capsLock = !capsLock;
-            return capsLock;
-          }),
-        keyPress$
-          .filter(e => {
-            var chr = getDisplayChar(e);
-            if (!chr || chr.toLowerCase() == chr.toUpperCase())
-              return false;
-            return true;
-          }).map(e => {
-            var chr = getDisplayChar(e);
-            capsLock = (chr.toLowerCase() == chr && e.shiftKey) || (chr.toUpperCase() == chr && !e.shiftKey);
-            return capsLock;
-          })
-      ).startWith(capsLock));
-    const shift$ = xs.create(shiftProducer);
-    const capsLock$ = xs.create(capsLockProducer);
-    const sinks = {
-      keyDown$,
-      keyUp$,
-      keyPress$,
-      shift$,
-      capsLock$
-    };
-    return sinks;
+    return new KeyboardDriver();
   }
   return keyboardDriver;
 }
